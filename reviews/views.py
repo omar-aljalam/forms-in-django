c79e4f617975a60f31f0d4d9fc24f3795e6adbd4
Omar <omar-aljalam@outlook.com>
2025-11-02 11:04:11 +0200

    Working with Froms!

---
+++ reviews/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+
+# Create your views here.
+
+def review(request):
+    if request.method == "POST":
+        return HttpResponseRedirect("/thank-you")
+    return render(request, "reviews/review.html")
+
+def thank_you(request):
+    return render(request, "reviews/thank_you.html")
