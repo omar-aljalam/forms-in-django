69c08a6160b4ec6a82971cc23fac7bfaefe76c38
Omar <omar-aljalam@outlook.com>
2025-11-07 10:42:34 +0200

    File Field

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    image = models.FileField(upload_to="images") # this file will NOT be stored in the DB since files should be stored in hard drives so this will store the path of the file after storing it somewhere in the drive
+    # Uploda to --> check project settings Media Root
--- profiles/views.py
+++ profiles/views.py
@@ -2,17 +2,26 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
 
-# Create your views here.
+from .forms import ProfileForm
+from .models import UserProfile
 
-def store_file(file):
-    with open("temp/image.jpg", "wb+") as f:
-        for chunk in file.chunks():
-            f.write(chunk)
+# Create your views here.
 
 class CreateProfileView(View):
     def get(self, request):
-        return render(request, "profiles/create_profile.html")
+        form = ProfileForm()
+        return render(request, "profiles/create_profile.html", {
+            "form": form
+        })
 
     def post(self, request):
-        store_file(request.FILES["image"])
-        return HttpResponseRedirect("/profiles")
+        submitted_form = ProfileForm(request.POST, request.FILES)
+
+        if submitted_form.is_valid():
+            profile = UserProfile(image=request.FILES["user_image"])
+            profile.save()
+            return HttpResponseRedirect("/profiles")
+        
+        return render(request, "profiles/create_profile.html", {
+        "form": submitted_form
+    })
