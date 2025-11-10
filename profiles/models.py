7b2ab43843581005c73fe29fdf0d83b2bb4025fa
Omar <omar-aljalam@outlook.com>
2025-11-10 03:45:28 +0200

    Image Field and Serving Uploaded Files

--- profiles/models.py
+++ profiles/models.py
@@ -3,5 +3,7 @@ from django.db import models
 # Create your models here.
 
 class UserProfile(models.Model):
-    image = models.FileField(upload_to="images") # this file will NOT be stored in the DB since files should be stored in hard drives so this will store the path of the file after storing it somewhere in the drive
-    # Uploda to --> check project settings Media Root
+    # image = models.FileField(upload_to="images") # this file will NOT be stored in the DB since files should be stored in hard drives so this will store the path of the file after storing it somewhere in the drive
+    image = models.ImageField(upload_to="images")
+
+    # # Uploda to --> check project settings Media Root
